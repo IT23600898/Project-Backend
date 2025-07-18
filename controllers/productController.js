66450b6cf07389208b24d7523ca0eb7d7087711e
U L R Kavindya <it23600898@my.sliit.lk>
2025-07-19 01:59:11 +0530

    Add the Get Product function and Create Product function 😊

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,25 @@
 import Product from "../models/products";
+import { isAdmin } from "./userController";
 
-export fuction 
+export function createProduct(req, res){
+    
+    if(!isAdmin(req)){
+        res.json({
+            message: "Please login as a administrator to add products"
+        })
+    }
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>{
+        res.json({
+            message: "Product Created."
+        })
+    }).catch((error)=>{
+        res.json({
+            message: error
+        })
+    })
+}
+
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,1 +1,8 @@
+import express from "express";
 import Product from "../models/products";
+import { createProduct, getProducts } from "../controllers/productController";
+
+const productRouter = express.Router();
+
+productRouter.post("/", createProduct);
+productRouter.get("/", getProducts)
