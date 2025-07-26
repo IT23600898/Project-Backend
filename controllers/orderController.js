bdfdb9ed0cb3d862840e877ea743040819408a8b
U L R Kavindya <it23600898@my.sliit.lk>
2025-07-26 15:00:13 +0530

    Create a auto generated the Order Id 🪛

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -6,6 +6,24 @@ export async function createOrder(res, req){
     try{
         const latestOrder = await Order.find().sort({date : -1}).limit(1)
 
+        let orderId
+
+        if(latestOrder.length == 0 ){
+            orderId = "CBC001"
+        }else{
+            const currentOrderId = latestOrder[0] / orderId
+
+            const numberString  = currentOrderId.replace("CBC", "")
+
+            const number = parseInt(numberString)
+
+            const newNumber = (number + 1).toString().padStart(4, "0");
+
+            orderId = "CBC" + newNumber
+        }
+
+        
+
         
 
     }catch(error){
